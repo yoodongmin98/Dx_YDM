5cebe8a8d3c69100a51537c129f749b77495c5a1
동민 유 <vpt515@gmail.com>
2023-06-13 21:52:10 +0900

    유동민 : Balloon Setting중

--- Thereisnogame/BalloonSecure.cpp
+++ Thereisnogame/BalloonSecure.cpp
@@ -1,1 +1,34 @@
 #include "PrecompileHeader.h"
+#include "BalloonSecure.h"
+#include "ActorTypeEnum.h"
+//Base
+//PlatForm
+//Core
+#include <GameEngineCore/GameEngineLevel.h>
+
+
+//Actor
+
+BalloonSecure::BalloonSecure()
+{
+}
+
+BalloonSecure::~BalloonSecure()
+{
+}
+
+void BalloonSecure::Start()
+{
+	
+}
+
+void BalloonSecure::Update(float _DeltaTime)
+{
+	
+}
+
+void BalloonSecure::Render(float _Delta)
+{
+
+};
+
--- Thereisnogame/BalloonSecure.h
+++ Thereisnogame/BalloonSecure.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <GameEngineCore/GameEngineActor.h>
+#include "G_ActorBase.h"
+
+class BalloonSecure : public G_ActorBase
+{
+public:
+	BalloonSecure();
+	~BalloonSecure();
+
+	BalloonSecure(const BalloonSecure& _Other) = delete;
+	BalloonSecure(BalloonSecure&& _Other) noexcept = delete;
+	BalloonSecure& operator=(const BalloonSecure& _Other) = delete;
+	BalloonSecure& operator=(BalloonSecure&& _Other) noexcept = delete;
+
+protected:
+	void Start();
+	void Update(float _Delta) override;
+	void Render(float _Delta) override;
+private:
+	std::shared_ptr<class GameEngineSpriteRenderer> BalloonSecures;
+	std::shared_ptr<class GameEngineCollision> BalloonSecuresCollision;
+};
+
