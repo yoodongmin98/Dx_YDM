e158fbc95f2aa8ab7ad9f57d0d0686431bc28b38
동민 유 <vpt515@gmail.com>
2023-06-28 19:28:07 +0900

    유동민 : Panel Switch Set

--- Thereisnogame/CoffreFortPanel.cpp
+++ Thereisnogame/CoffreFortPanel.cpp
@@ -14,12 +14,13 @@ CoffreFortPanel::~CoffreFortPanel()
 
 void CoffreFortPanel::Start()
 {
-	CoffreFortPanels = Init(CoffreFortPanels, "CoffreFortPanel.png", { 390,285 }, float4::Zero+float4::Right*64);
+	CoffreFortPanels = Init(CoffreFortPanels, "CoffreFortPanel.png", { 390,285 }, float4::Zero + float4::Right * 64);
+	CoffreFortPanelsColliision = CollisionInit(CoffreFortPanelsColliision, { 80,150 }, float4::Right * 210);
 }
 
 void CoffreFortPanel::Update(float _DeltaTime)
 {
-
+	PanelInterCheck(_DeltaTime);
 }
 
 void CoffreFortPanel::Render(float _Delta)
@@ -27,3 +28,23 @@ void CoffreFortPanel::Render(float _Delta)
 
 };
 
+void CoffreFortPanel::PanelInterCheck(float _DeltaTime)
+{
+	PanelTime += _DeltaTime;
+	if (true == ClickCheck(CoffreFortPanelsColliision))
+	{
+		PanelOpen = !PanelOpen;
+		PanelTime = 0.0f;
+		StartPosX = CoffreFortPanels->GetTransform()->GetLocalPosition();
+	}
+	if (true == PanelOpen)
+	{
+		EndPosX = { 300,0,0 };
+		CoffreFortPanels->GetTransform()->SetLocalPosition(float4::LerpClamp(StartPosX, EndPosX, PanelTime));
+	}
+	else
+	{
+		EndPosX = {64,0,0};
+		CoffreFortPanels->GetTransform()->SetLocalPosition(float4::LerpClamp(StartPosX, EndPosX, PanelTime));
+	}
+}
--- Thereisnogame/CoffreFortPanel.h
+++ Thereisnogame/CoffreFortPanel.h
@@ -12,11 +12,20 @@ public:
 	CoffreFortPanel(CoffreFortPanel&& _Other) noexcept = delete;
 	CoffreFortPanel& operator=(const CoffreFortPanel& _Other) = delete;
 	CoffreFortPanel& operator=(CoffreFortPanel&& _Other) noexcept = delete;
+	void PanelInterCheck(float _DeltaTime);
 protected:
 	void Start();
 	void Update(float _Delta) override;
 	void Render(float _Delta) override;
 private:
 	std::shared_ptr<class GameEngineSpriteRenderer> CoffreFortPanels;
+	std::shared_ptr<class GameEngineSpriteRenderer> TestCoffreFortPanels;
+	std::shared_ptr<class GameEngineCollision> CoffreFortPanelsColliision;
+
+	bool PanelOpen = false;
+	float PanelTime = 0.0f;
+
+	float4 StartPosX = float4::Zero;
+	float4 EndPosX = float4::Zero;
 };
 
