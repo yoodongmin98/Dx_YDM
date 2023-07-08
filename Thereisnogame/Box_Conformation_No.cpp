091261318ffd74b88c4ddcf3252729122800c932
yoodongmin98 <vpt515@gmail.com>
2023-07-08 15:28:40 +0900

    유동민 : Menu_Quit Set

--- Thereisnogame/Box_Conformation_No.cpp
+++ Thereisnogame/Box_Conformation_No.cpp
@@ -16,9 +16,9 @@ Box_Conformation_No::~Box_Conformation_No()
 
 void Box_Conformation_No::Start()
 {
-	float4 Position = { 200,-300,0 };
+	float4 Position = { 230,-300,0 };
 	Boxs = Init(Boxs, "MenuOptions_Confirmation_No.png", { 273 * 0.7f,309 * 0.7f,1 }, Position);
-	BoxsCollision = CollisionInit(BoxsCollision, { 150,50,1 }, Position + float4::Up * 100);
+	BoxsCollision = CollisionInit(BoxsCollision, { 150,50,1 }, Position + float4::Up * 60);
 }
 
 void Box_Conformation_No::Update(float _DeltaTime)
@@ -26,12 +26,12 @@ void Box_Conformation_No::Update(float _DeltaTime)
 	ManagedCollision(BoxsCollision, 1);
 	if (true == ClickCheck(BoxsCollision))
 	{
+		Play(Sound, "MainMenu_Click.wav", 0.1f);
 		BoxsCollision->Death();
 		IsClick = true;
 	}
 	if (true == IsClick)
 	{
-		Play(Sound, "MainMenu_Click.wav", 0.1f);
 		Boxs->GetTransform()->AddLocalPosition({ 0,-500 * _DeltaTime,0 });
 		if (Boxs->GetTransform()->GetLocalPosition().y < -800.0f)
 		{
